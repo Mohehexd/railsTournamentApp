74c3f09875fd0ddfa7b9d6417c48198b2d6eb361
mohehexd <dumblemoe@gmail.com>
2020-02-17 21:48:48 -0600

    Creating player index page routed to player index controller.

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,2 +1,29 @@
 class PlayersController < ApplicationController
+    def index
+        @player = Player.all
+    end 
+
+    def create
+        @player = Player.new
+    end 
+
+    def new
+        @player = Player.new 
+    end
+
+    def show
+
+    end
+
+    def edit
+
+    end
+
+    def update
+
+    end
+
+    def destroy
+
+    end
 end
