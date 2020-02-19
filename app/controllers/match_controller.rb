d390e310eca965bb50850e5736bc6b7dc49accd9
mohehexd <dumblemoe@gmail.com>
2020-02-19 17:09:58 -0600

    Creating a user model.

--- app/controllers/match_controller.rb
+++ app/controllers/match_controller.rb
@@ -1,13 +1,22 @@
 class MatchController < ApplicationController
+    
+    #Get /matches
     def index
         @match = Match.all
     end
 
-    def create
-        
+    #Get /matches/1
+    def create 
     end
 
+    #Get /matches/new
     def new
         @match = Match.new
     end
+
+    #Get /matches/1/edit
+    def edit
+    end
+
+    
 end
