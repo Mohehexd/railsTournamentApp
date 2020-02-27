a042826d50bb59c206ad41bc6a8e40d482481b66
mohehexd <dumblemoe@gmail.com>
2020-02-27 13:04:08 -0600

    Building params for create in match controller (not complete).

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -1,5 +1,5 @@
 class MatchesController < ApplicationController
-    
+    before_action :set_match, only:[:show, :edit, :update, :destroy]
     #show /show all page
     def index
         @match = Match.all
@@ -7,17 +7,18 @@ class MatchesController < ApplicationController
 
     #Get /read action for a single object
     def show 
-        @match = Match.find_by(id: params[:id])
+        @match = Match.find_by(id:params[:id])
     end     
 
     #Get /create action
     def create 
+        byebug
         @match = Match.new(match_number: params[:match_number], match_date: params[:match_date] )
         if @match.valid?
             @match.save
-            redirect_to new_match_path
-        else
             redirect_to match_path(@match.id)
+        else
+            redirect_to new_match_path, notice: 'Something went wrong Mo!'
         end
     end
 
@@ -38,5 +39,13 @@ class MatchesController < ApplicationController
     def destroy 
     end
 
+    private
+    def match_params
+        params.require(:match).permit(:match_number, :match_params, :match_id)
+    end
+       
+    def set_match
+        @match = Match.find_by(id:params[:match_id])
+    end
     
 end
