2d2b998ed3777a390c39decd5ae888b1f854985c
mohehexd <dumblemoe@gmail.com>
2020-02-28 16:16:52 -0600

    Adding has_many, has_many :through and belongs to associations to match, player and user models.

--- app/models/match.rb
+++ app/models/match.rb
@@ -1,4 +1,7 @@
 class Match < ApplicationRecord
+    has_many :players
+    has_many :users, through: :players
+
     validates :match_number, presence:{message: "A match must have a number!"}
     validates :match_date, presence:true 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
+    has_many :players
+    has_many :matches, through: :players
 
     has_secure_password
-
 end
